c04092ed23daca754b8707b2ffad1ca0c1e44d8e
sjackson01 <stevenpjackson0@gmail.com>
2020-03-09 09:34:20 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php # Script 16.1- mysqli_oop_connect.php
+// This file contains the databse access information. 
+// This file also establishes a connection to MySQL,
+// selects the databse, and sets the encoding. 
+// The MySQL interactions use OOP!
